d4153d86ef32b5549b5039d9b5709d31cb30fedf
Nicolas Hoffmann <hoffmann94@gmail.com>
2023-12-03 22:28:25 -0300

    prepared vite config for multiple html entries

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -24,4 +25,12 @@ export default defineConfig({
       }
     }
   }
+  // build: {
+  //   rollupOptions: {
+  //     input: {
+  //       main: resolve(__dirname, 'index.html'),
+  //       admin: resolve(__dirname, 'admin/index.html')
+  //     }
+  //   }
+  // }
 })
