7fed32737633abfdc934891876aa2bb8a229557f
Nicolas Hoffmann <hoffmann94@gmail.com>
2023-12-31 17:44:01 -0300

    added view mixin

--- src/modules/_common/mixins/view-mixin.js
+++ src/modules/_common/mixins/view-mixin.js
@@ -1,5 +1,8 @@
 const viewMixin = {
   methods: {
+    /**
+     * Updates the --navbar-height CSS variable based on the element nav.navigation height
+     */
     updateNavbarHeightCSSVariable() {
       const navbarHeight = document.querySelector('.navigation').offsetHeight
       const root = document.querySelector(':root')
