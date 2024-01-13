924852a16be5b1db24a2c46e72c339077ba53530
Nicolas Hoffmann <hoffmann94@gmail.com>
2024-01-13 18:11:41 -0300

    added themes e2e testing

---
+++ cypress/e2e/themes.cy.js
@@ -0,0 +1,42 @@
+import ThemeEnum from '../../src/domain/enums/ThemeEnum'
+import SubscriptionTypeEnum from '../../src/domain/enums/SubscriptionTypeEnum'
+
+describe('Themes', () => {
+  beforeEach(function () {
+    cy.fixture('user').then((user) => {
+      this.user = user
+    })
+
+    cy.intercept({
+      pathname: '/user/*'
+    }).as('fetchUser')
+  })
+
+  it('Checks if checkout has professional theme', function () {
+    cy.visit('/')
+
+    cy.get('[data-cy="body"]').should('have.class', `system--${ThemeEnum.PROFESSIONAL}`)
+
+    cy.get(`[data-cy="subscription-type-button-${SubscriptionTypeEnum.PROFESSIONAL}"]`).click()
+
+    cy.fillEmail(this.user.email)
+
+    cy.wait('@fetchUser')
+
+    cy.get('[data-cy="body"]').should('have.class', `system--${ThemeEnum.PROFESSIONAL}`)
+  })
+
+  it('Checks if checkout has student theme', function () {
+    cy.visit('/')
+
+    cy.get('[data-cy="body"]').should('have.class', `system--${ThemeEnum.PROFESSIONAL}`)
+
+    cy.get(`[data-cy="subscription-type-button-${SubscriptionTypeEnum.STUDENT}"]`).click()
+
+    cy.fillEmail(this.user.email)
+
+    cy.wait('@fetchUser')
+
+    cy.get('[data-cy="body"]').should('have.class', `system--${ThemeEnum.STUDENT}`)
+  })
+})
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,10 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('fillEmail', (email) => {
+  cy.get('[data-cy="email-modal"]').should('be.visible')
+
+  cy.get('[data-cy="email-input"]').type(email)
+  cy.get('[data-cy="email-form-submit"]').click()
+})
