eadaa1d4d0ecfb7b4bb296d6a7d73d54996a18b5
Nicolas Hoffmann <hoffmann94@gmail.com>
2024-01-14 22:44:30 -0300

    fixed cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
-import { defineConfig } from 'cypress'
+// eslint-disable-next-line no-undef
+const { defineConfig } = require('cypress')
 
-export default defineConfig({
+// eslint-disable-next-line no-undef
+module.exports = defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
     baseUrl: 'http://localhost:4173'
