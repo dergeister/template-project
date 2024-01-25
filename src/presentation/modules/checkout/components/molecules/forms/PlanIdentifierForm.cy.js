53382e91f5ca3e2ac47bdf0afc69f1c46419b03d
Nicolas Hoffmann <hoffmann94@gmail.com>
2024-01-24 23:33:35 -0300

    fixed plan identifier form test

--- src/presentation/modules/checkout/components/molecules/forms/PlanIdentifierForm.cy.js
+++ src/presentation/modules/checkout/components/molecules/forms/PlanIdentifierForm.cy.js
@@ -1,7 +1,7 @@
 import PlanIdentifierForm from './PlanIdentifierForm.vue'
 import { setActivePinia, createPinia } from 'pinia'
 
-import { planIdentifierPerSubscriptionType } from '@services/payment-service'
+import PaymentService from '@services/payment-service'
 import SubscriptionTypeEnum from '@enums/SubscriptionTypeEnum'
 import usePaymentStore from '@store/payment'
 
@@ -15,7 +15,8 @@ describe('Payment Method Form', () => {
   it('Validates professional plan', function () {
     cy.mount(PlanIdentifierForm)
 
-    const plans = planIdentifierPerSubscriptionType[SubscriptionTypeEnum.PROFESSIONAL]
+    const plans =
+      PaymentService.planIdentifierPerSubscriptionType[SubscriptionTypeEnum.PROFESSIONAL]
 
     for (let i = 0; i < plans.length; i++) {
       const plan = plans[i]
@@ -30,7 +31,7 @@ describe('Payment Method Form', () => {
     const paymentStore = usePaymentStore()
     paymentStore.subscriptionType = SubscriptionTypeEnum.STUDENT
 
-    const plans = planIdentifierPerSubscriptionType[SubscriptionTypeEnum.STUDENT]
+    const plans = PaymentService.planIdentifierPerSubscriptionType[SubscriptionTypeEnum.STUDENT]
 
     for (let i = 0; i < plans.length; i++) {
       const plan = plans[i]
